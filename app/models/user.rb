942f62382f33aea5b6cf46e10918fa68ceb851f0
Chinmay Garde <chinmaygarde@gmail.com>
2010-09-28 12:27:34 -0400

    Added User Spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,4 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
-  
-  def useless_function
-    a = 1 + 2
-    b = 'Hello World'
-  end
 end
