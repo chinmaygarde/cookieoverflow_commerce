3e318561a3ce5c23630bebdb9c506e9df61d2043
Chinmay Garde <chinmaygarde@gmail.com>
2010-09-28 12:24:48 -0400

    Added User factory

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
+  
+  def useless_function
+    a = 1 + 2
+    b = 'Hello World'
+  end
 end
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -2,3 +2,8 @@ Factory.define :bid_item do |b|
   b.title "iPhone 4G"
   b.body  "The coolest phone ever"
 end
+
+Factory.define :user do |u|
+  u.email     "anonymous@example.com"
+  u.password  "secret"
+end
