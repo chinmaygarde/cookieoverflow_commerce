45143dfc564fb25ca4580ce3472571eae2132ffb
Chinmay Garde <chinmaygarde@gmail.com>
2010-10-10 17:00:13 -0400

    Added seed value for user with admin authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 Role.create(:title => "admin")
 Role.create(:title => "buyer")
 Role.create(:title => "seller")
+
+u = User.create(:email => "admin@admin.com", :password => "password")
+u.update_roles("admin")
