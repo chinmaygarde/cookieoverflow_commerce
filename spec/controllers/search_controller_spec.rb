ef936f01c1134146d893c176545d2c95927d3fb5
Chinmay Garde <chinmaygarde@gmail.com>
2010-10-26 03:25:42 -0400

    Too many daemons. We need divine intervention. Next task has to be implementing god or monit. Stubbed out Sphinx calls.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe SearchController do
-
+  def mock_bid_item(stubs={})
+    @mock_bid_item ||= mock_model(BidItem, stubs)
+  end
   describe "GET 'bid_item'" do
     it "should be successful" do
+      BidItem.stub(:search).and_return([mock_bid_item(:title => "Foo")])
       get 'bid_item', :q => "Buzzy"
       response.should be_success
     end
