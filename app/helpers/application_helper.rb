408fe51e66239b95007e2f21694d0788518c3fa8
Chinmay Garde <chinmaygarde@gmail.com>
2010-10-26 10:52:53 -0400

    Added tests for gravatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,3 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def gravatar(arg)
     if arg.kind_of? User
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+include ApplicationHelper
+
+describe ApplicationHelper do
+  it "should construct a gravatar url given an email" do
+    gravatar("admin@admin.com").should == "http://www.gravatar.com/avatar/64e1b8d34f425d19e1ee2ea7236d3028.jpg"
+  end
+  it "should construct a gravatar url given a user" do
+    u = Factory(:user, :email => "admin@admin.com")
+    gravatar(u).should == "http://www.gravatar.com/avatar/64e1b8d34f425d19e1ee2ea7236d3028.jpg"
+  end
+  it "should construct a gravatar url given some junk object" do
+    u = Factory(:bid_item)
+    gravatar(u).class.should == String
+  end
+end
